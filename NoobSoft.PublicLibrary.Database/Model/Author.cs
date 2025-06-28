05a97ce15abe60ea9b6ada77ff97adabd08b10e4
Helga Rykov Ibsen <helgarykovibsen@gmail.com>
2025-06-28 15:15:02 +0200

    Modified fields and constructor

--- NoobSoft.PublicLibrary.Database/Model/Author.cs
+++ NoobSoft.PublicLibrary.Database/Model/Author.cs
@@ -2,18 +2,10 @@ namespace NoobSoft.PublicLibrary.Database.Model;
 
 public class Author : Person
 {
-    public Guid id { get; }
-    private string name { get; }
-    private DateTime birthday { get; }
-    private List<Book> myBooks { get; }
-    public object Id { get; }
-    public object Name { get; }
-    public object Birthday { get; }
+    private List<Book> loanedBooks { get; set; }
 
     public Author(Guid id, string name, DateTime birthday) : base(id, name, birthday)
     {
-        this.id = id;
-        this.name = name;
-        this.birthday = birthday;
+        loanedBooks = new List<Book>();
     }
 }
