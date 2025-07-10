87a423b8f09aecf6325d2d9f27604e5fb97abd00
Helga Rykov Ibsen <helgarykovibsen@gmail.com>
2025-07-10 14:14:14 +0200

    Add LoadData() to LibService

--- NoobSoft.PublicLibrary.Database/Business/LibraryService.cs
+++ NoobSoft.PublicLibrary.Database/Business/LibraryService.cs
@@ -1,3 +1,6 @@
+using NoobSoft.PublicLibrary.Database.Model;
+using NoobSoft.PublicLibrary.Database.Repository;
+
 namespace NoobSoft.PublicLibrary.Database.Business;
 
 /// <summary>
@@ -7,5 +10,56 @@ namespace NoobSoft.PublicLibrary.Database.Business;
 /// </summary>
 public class LibraryService : ILibraryService
 {
-    
+    private readonly ILibraryRepository _repository;
+    private readonly ITimeProvider _timeProvider;
+
+    public LibraryService(ILibraryRepository repo, ITimeProvider timeProvider)
+    {
+        _repository = repo;
+        _timeProvider = timeProvider;
+    }
+    public bool TryLoanBook(Guid bookId, Guid loanerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ReturnBook(Guid bookId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal CalculateFeeForLoaner(Guid loanerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Book> GetBooksLoanedByPerson(Guid loanerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Loan> GetLoansForBook(Guid bookId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ReserveBook(Guid bookId, Guid loanerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Guid> GetReservationsForBook(Guid bookId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Book> GetMostPopularBooks(int top = 5)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LoadData()
+    {
+        _repository.LoadData();
+    }
 }
