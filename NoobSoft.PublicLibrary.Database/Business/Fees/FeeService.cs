690d6ae6d97853871684d50765bc483e1f962359
Helga Rykov Ibsen <helgarykovibsen@gmail.com>
2025-08-10 11:35:14 +0200

    Preliminary FeeService

--- NoobSoft.PublicLibrary.Database/Business/Fees/FeeService.cs
+++ NoobSoft.PublicLibrary.Database/Business/Fees/FeeService.cs
@@ -1,6 +1,46 @@
+using NoobSoft.PublicLibrary.Database.Model;
+
 namespace NoobSoft.PublicLibrary.Database.Business.Fees;
 
-public class FeeService
+public class FeeService : IFeeService
 {
-    
+    public FeeAssessment AssessLoan(Loan loan, DateTime asOf)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsLoanLost(Loan loan, DateTime asOf)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int OverdueDays(Loan loan, DateTime asOf)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal GetOutstandingDebt(Guid loanerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsSuspended(Guid loanerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public LedgerEntry PostReturn(Loan loan, DateTime returnedAt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public PaymentReceipt RecordPayment(Guid loanerId, decimal amount, DateTime when)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IReadOnlyList<LedgerEntry> GetLedger(Guid loanerId)
+    {
+        throw new NotImplementedException();
+    }
 }
